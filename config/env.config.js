139921129d96b796cf98da83e08b7a0524084930
xr <1697049214@qq.com>
2023-11-22 10:30:18 +0800

    init

---
+++ config/env.config.js
@@ -0,0 +1,22 @@
+export default {
+    VUE_REQUEST_TIMEOUT: 10000,
+    development: {
+      NODE_ENV: "development",
+      VUE_APP_BASE_API_SUFFIX: "api",
+      VUE_APP_BASE_API: 'https://ycq.stttttt.com',
+      WEB_VIEW_URL: ''
+    },
+    production: {
+      NODE_ENV: "production",
+      VUE_APP_BASE_API_SUFFIX: "api",
+      VUE_APP_BASE_API: 'https://ycq.stttttt.com',
+      WEB_VIEW_URL: ''
+    },
+    test: {
+      NODE_ENV: "test",
+      VUE_APP_BASE_API_SUFFIX: "api",
+      VUE_APP_BASE_API: 'https://ycq.stttttt.com',
+      WEB_VIEW_URL: ''
+    },
+  };
+  
