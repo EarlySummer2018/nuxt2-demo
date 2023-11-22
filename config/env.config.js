396738aea74b9b4af8226120a7c28714b17653d8
xr <1697049214@qq.com>
2023-11-22 10:51:40 +0800

    添加 env

--- config/env.config.js
+++ config/env.config.js
@@ -3,19 +3,19 @@ export default {
     development: {
       NODE_ENV: "development",
       VUE_APP_BASE_API_SUFFIX: "api",
-      VUE_APP_BASE_API: 'https://ycq.stttttt.com',
+      VUE_APP_BASE_API: 'https://ycq.stttttt.com1',
       WEB_VIEW_URL: ''
     },
     production: {
       NODE_ENV: "production",
       VUE_APP_BASE_API_SUFFIX: "api",
-      VUE_APP_BASE_API: 'https://ycq.stttttt.com',
+      VUE_APP_BASE_API: 'https://ycq.stttttt.com2',
       WEB_VIEW_URL: ''
     },
     test: {
       NODE_ENV: "test",
       VUE_APP_BASE_API_SUFFIX: "api",
-      VUE_APP_BASE_API: 'https://ycq.stttttt.com',
+      VUE_APP_BASE_API: 'https://ycq.stttttt.com3',
       WEB_VIEW_URL: ''
     },
   };
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,8 @@
 <script>
 export default {
   name: 'IndexPage',
+  created() {
+    console.log(process.env.VUE_APP_BASE_API);
+  },
 }
 </script>
